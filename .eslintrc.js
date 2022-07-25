c32b0630afe9962da1423aa266540c6b224be798
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-07-25 15:44:56 +0200

    eslint scripts // prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,20 @@
+
+const settings = [
+  "plugin:vue/vue3-essential",
+  "eslint:recommended",
+  "@vue/typescript/recommended",
+];
+
+if (process.env.NODE_ENV === "production") {
+  settings.push("plugin:prettier/recommended")
+}
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: settings,
   parserOptions: {
     ecmaVersion: 2020,
   },
