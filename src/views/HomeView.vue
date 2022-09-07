430c4a3b7453cc129211d4583ebbf58684bb248d
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-09-07 10:13:15 +0200

    show matchmaking if connected

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,16 @@
   <br><br>
   <div class="row w-100 d-flex justify-content-around">
     <div class="col-12 col-lg-10 col-xl-8">
-      <PlayMenu />
+      <PlayMenu v-if="isConnected" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import PlayMenu from "@/components/PlayMenu.vue";
+import { computed } from "@vue/runtime-core";
+import { store } from "@/store"
 
+const isConnected = computed(() => store.getters["auth/isConnected"]);
 
 </script>
