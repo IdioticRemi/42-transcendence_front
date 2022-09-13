922a7c5335021cc9b8664fd158121bf10cd990c7
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-09-13 11:32:29 +0200

    refresh leaderboard

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="mt-4">
-		<h1 class="mb-3">Leaderboard <i class="bi bi-trophy-fill text-warning ms-2"/></h1>
+		<div class="d-flex justify-content-between w-100">
+			<h1 class="mb-3">Leaderboard <i class="bi bi-trophy-fill text-warning ms-2"/></h1>
+			<button class="btn btn-primary mb-3" :key="refresh" @click="refreshGames()">Refresh
+          		<i class="bi bi-arrow-repeat ms-1" />
+        	</button>
+		</div>
 		<div v-for="(user, rank) in lb" class="card mb-2" :key="user.id">
 			<div class="card-header d-flex justify-content-between w-100">
 				<h2 :class="`card-title m-0 text-${getColor(rank + 1, 'rank')}`">
@@ -32,14 +37,15 @@
 import { sendBackendRequest } from "@/utils/user";
 import { ref, onMounted } from "vue";
 
+const refresh = ref(false);
 const lb = ref(null);
 
 function getColor(n: number, type: string) {
 	console.log(n);
 	if (type === 'wr') {
-		if (n > 70) return 'success';
-		if (n > 50) return 'primary';
-		if (n > 30) return 'warning';
+		if (n > 65) return 'success';
+		if (n > 48) return 'primary';
+		if (n > 40) return 'warning';
 		return 'danger';
 	} else if (type === 'rank') {
 		if (n === 1) return 'gold';
@@ -53,6 +59,7 @@ async function refreshGames() {
 	const r = await sendBackendRequest('/users/leaderboard');
 	if (r.status === 'success')
 		lb.value = r.payload;
+	refresh.value = !refresh.value;
 }
 
 onMounted(() => refreshGames());
