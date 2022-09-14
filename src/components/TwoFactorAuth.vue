2d6dc1fc689abb5cd824953fb2ad7a6af69c94d4
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-09-14 11:21:08 +0200

    qrcode reception ok

--- src/components/TwoFactorAuth.vue
+++ src/components/TwoFactorAuth.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex-col justify-content-end">
-		<button v-if="twoFactorEnabled" class="btn btn-danger" @click="disable2fa()">Disable 2FA</button>
+		<button v-if="twoFactorEnabled" class="btn btn-sm btn-danger" @click="disable2fa()">Disable 2FA</button>
 		<button v-else class="btn btn-sm btn-primary my-2 " @click="enable2fa()">Enable 2FA</button>
 
 		<img v-if="qrcode" :src="qrcodeURI" alt="qrcode">
@@ -17,15 +17,16 @@ const qrcodeURI = ref("");
 const qrcode = ref(false);
 
 async function enable2fa() {
-	const r = await sendBackendRequest('auth/generate', {method: 'POST'});
+	const r = await sendBackendRequest('/auth/generate', {method: 'POST'});
 	if (r.status === 'success') {
-		qrcodeURI.value = r;
+		qrcodeURI.value = r.payload;
 		qrcode.value = true;
+		store.state.auth.user!.otp_enabled = true;
 	}
 }
 
 async function disable2fa() {
-	const r = await sendBackendRequest('auth/disable-2fa', {method: 'POST'});
+	const r = await sendBackendRequest('/auth/disable-2fa', {method: 'POST'});
 	if (r.status === 'success')
 		store.state.auth.user!.otp_enabled = false; //TODO mutation ??
 }
