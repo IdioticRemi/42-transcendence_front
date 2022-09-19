d3ef8e2253d431b2ca8cc3b2728d29da5c1c85a8
mdesoeuv <mehdi.desoeuvre@gmail.com>
2022-09-19 16:22:23 +0200

    ssl certification

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,15 @@
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
+
+const fs = require('fs');
+
+const key = fs.readFileSync('./cert/key.pem');
+const cert = fs.readFileSync('./cert/cert.pem');
+
 module.exports = {
 	devServer: {
-        port: 8081
+        port: 8081,
+        https: {key, cert},
     }
 };
